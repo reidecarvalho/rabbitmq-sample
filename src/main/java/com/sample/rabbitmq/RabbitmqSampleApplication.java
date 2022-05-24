c6f6e8bbdd77e8a40aebeca0d01c6b538c607769
reidecarvalho <reidecarvalho@gmail.com>
2022-05-24 09:22:29 -0300

    Created a Producer to test

--- src/main/java/com/sample/rabbitmq/RabbitmqSampleApplication.java
+++ src/main/java/com/sample/rabbitmq/RabbitmqSampleApplication.java
@@ -1,13 +1,21 @@
 package com.sample.rabbitmq;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class RabbitmqSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqSampleApplication.class, args);
+	public static void main(String[] args) throws IOException, TimeoutException {
+		AnnotationConfigApplicationContext ctx = (AnnotationConfigApplicationContext) SpringApplication.run(RabbitmqSampleApplication.class, args);
+
+		Producer producer = ctx.getBean(Producer.class);
+		producer.send("Enviando info: " + LocalDateTime.now());
 	}
 
 }
